21da86e0d0e991c913d467e147e0a61823692aa4
coyote321 <cochen8@s.sfusd.edu>
2025-12-10 13:24:58 -0800

    M9L8

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -1,5 +1,5 @@
+class_name Runner
 extends CharacterBody2D
-
 
 @export var max_speed := 600.0
 @export var acceleration := 1000.0
@@ -7,6 +7,7 @@ extends CharacterBody2D
 
 @onready var _runner_visual: RunnerVisual = %RunnerVisualRed
 @onready var _dust: GPUParticles2D = %Dust
+signal walked_to
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -32,3 +33,24 @@ func _physics_process(delta: float) -> void:
 	else:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
 		_dust.emitting = false
+func walk_to(destination_global_position: Vector2) -> void:
+	# obtain the direction and angle
+	var direction := global_position.direction_to(destination_global_position)
+	_runner_visual.angle = direction.orthogonal().angle()
+
+	# Set the proper animation name
+	_runner_visual.animation_name = RunnerVisual.Animations.WALK
+	_dust.emitting = true
+
+	# obtain distance, and calculate direction from that
+	var distance := global_position.distance_to(destination_global_position)
+	var duration :=  distance / (max_speed * 0.2)
+
+	# tween the runner to destination, then emit `walked_to`
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", destination_global_position, duration)
+	tween.finished.connect(func():
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_dust.emitting = false
+		walked_to.emit()
+)
